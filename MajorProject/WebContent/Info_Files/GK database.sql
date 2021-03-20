f1db0ace059f3a0b1eb49cdc17fda3a66f9f682f
sumitvyas786 <sumitvyas786@gmail.com>
2021-03-21 03:50:48 +0530

    .

--- MajorProject/WebContent/Info_Files/GK database.sql
+++ MajorProject/WebContent/Info_Files/GK database.sql
@@ -2,6 +2,10 @@ create database major_project_snake_n_ladder;
 
 use major_project_snake_n_ladder;
 
+desc General_Knowledge;
+select count(*) from General_Knowledge;
+select * from General_Knowledge;
+
 create table General_Knowledge (Id int(3) primary key  , 
 Stage varchar(1) not null,
 Question varchar(500) not null,
@@ -12,12 +16,7 @@ Option_4 varchar(200)not null,
 Answer varchar(200) not null
 );
 
-select count(*) from General_Knowledge;
-
-desc General_Knowledge;
-
-select * from General_Knowledge;
-
+-- ======================== Easy ========================
 insert into General_Knowledge values("1","E","Select consonants within the English alphabet?","100","40","21","26","21");
 insert into General_Knowledge values("2","E","What grows quicker—hair or toenails?","Hair","nails","legs","hands","Hair");
 insert into General_Knowledge values("3","E","Which is the tallest mountain in the world?","MountMcom","Mount Everest","Kalsubai","Himalayas","Mount Everest");
@@ -26,10 +25,10 @@ insert into General_Knowledge values("5","E","Which two parts of the body contin
 insert into General_Knowledge values("6","E","What is the next number in the following sequence– 7, 14, 21, 28?","35","29","30","i am weak in maths","35");
 insert into General_Knowledge values("7","E","How many zeros are there in one billion?","9","11","10","13","9");
 insert into General_Knowledge values("8","E","What does friction produce?","Heat","Spark","Nothing","BothA&B","Heat");
-insert into General_Knowledge values("9","E"," What is the total distance around a figure?","Circumference of surface","Perimeter of a surface ","diagonal to the durface","Radius of surface","Perimeter of a surface ");
+insert into General_Knowledge values("9","E","What is the total distance around a figure?","Circumference of surface","Perimeter of a surface","Diagonal to the durface","Radius of surface","Perimeter of a surface");
 insert into General_Knowledge values("10", "E","Who invented the letter zero?","Gagabhatta","Vibhishan","Aryabhatta","Newton","Aryabhatta");
 insert into General_Knowledge values("11", "E","How many cards are there in a playing deck?","53","52","49","50","52");
-insert into General_Knowledge values("12", "E"," Numbers that can be divided only by 1 or themselves are called?","Whole Number","Duplex Number","Prime Number","Rational Number","Prime Number");
+insert into General_Knowledge values("12", "E","Numbers that can be divided only by 1 or themselves are called?","Whole Number","Duplex Number","Prime Number","Rational Number","Prime Number");
 insert into General_Knowledge values("13", "E","What is the longest side of a right-angle triangle called?","Radius","Hypotaneous","Diameter","Median","Hypotaneous");
 insert into General_Knowledge values("14", "E","Largest Mammal in the world?","Shark","Elephant","Rat","Blue Whale","Blue Whale");
 insert into General_Knowledge values("15", "E","Which color symbolizes peace?","Pink","Green","White","Yellow","White");
@@ -39,9 +38,10 @@ insert into General_Knowledge values("18", "E","First Lady IPS officer in INDIA?
 insert into General_Knowledge values("19", "E","Man with most trophies in cricket?","Mahendra Singh Dhoni","Saurav Gnaguly","Kapil Dev","Azharuddin","Mahendra Singh Dhoni");
 insert into General_Knowledge values("20","E","Which famous children’s book has two characters named Tweedledum and Tweedledee?","Alice, Through the Looking-Glass by Lewis Carroll ","Tom and jerry","Cliford","Explore","Alice, Through the Looking-Glass by Lewis Carroll");
 
+-- ======================== Medium ========================
 insert into General_Knowledge values("21","M","Which of the following is not a metal?","gold","resin","glass","cu","resin");
-insert into General_Knowledge values("22","M","What is used by the driver to determine the direction of running the car?","Steering wheel","Linkage","CrossBar","Radiator","steering wheel");
-insert into General_Knowledge values("23","M"," Is the temperature of the moon higher or lower during the day?","High","Higher","Low","Lower","Higher");
+insert into General_Knowledge values("22","M","What is used by the driver to determine the direction of running the car?","Steering wheel","Linkage","CrossBar","Radiator","Steering wheel");
+insert into General_Knowledge values("23","M","__Is the temperature of the moon higher or lower during the day?","High","Higher","Low","Lower","Higher");
 insert into General_Knowledge values("24","M","Which planet is the smallest?","Neptune","Mars","Mercury","Earth","Mercury");
 insert into General_Knowledge values("25","M","“Number rules the Universe” Who said it?","Pythagoras","Archimedes","Charushila","BillGates","Pythagoras");
 insert into General_Knowledge values("26","M","Which movie is based on Srinivasa Ramanujan?","The Man Who Knew Infinity","Dont know","Mirzapur","The Numbers","The Man Who Knew Infinity");
@@ -63,21 +63,21 @@ insert into General_Knowledge values("41", "M","Which animal is also known as th
 insert into General_Knowledge values("42", "M","What toy does Arnold Schwarzenegger's son want in the movie 'Jingle All the Way'?","Bushwood Country Club","Turbo Man","Golf around world","Dingo","Turbo Man");
 insert into General_Knowledge values("43", "M","How many seconds does an NBA team have to take a shot that hits the rim?","24 seconds","20 seconds","A minute","30seconds","24 seconds");
 insert into General_Knowledge values("44","M","Which is the largest plateau in the world?","ChinaPlateau","Tibetan Plateau","MaharashtrianPlateau","SouthPla","Tibetan Plateau");
-insert into General_Knowledge values("45","M"," In which ocean did the famous Titanic sink in 1912— Pacific, North Atlantic, or the Mediterranean?","North Atlantic Ocean","Tibetan Plateau","MaharashtrianPlateau","SouthPla","North Atlantic Ocean");
+insert into General_Knowledge values("45","M","In which ocean did the famous Titanic sink in 1912— Pacific, North Atlantic, or the Mediterranean?","North Atlantic Ocean","Tibetan Plateau","MaharashtrianPlateau","SouthPla","North Atlantic Ocean");
 insert into General_Knowledge values("46","M","What makes up (approx.) 80% of our brain’s volume?","Nose and ears","Water","MaharashtrianPlateau","SouthPla","MaharashtrianPlateau");
 insert into General_Knowledge values("47","M","Which of these two lock horns in the stock market?","Bulls and bears","Trader","Bargainer","Stocker","Bulls and bears");
 insert into General_Knowledge values("48","M","Water in plants is transported by","Xylem","Stem","leaves","flowers","Xylem");
 insert into General_Knowledge values("49","M","What is not a component of chlorophyll?","Nitrogen","Calcium","leaves","flowers","Calcium");
 insert into General_Knowledge values("50","M","Onion (Allium cepa)is a modified form of?","Stem","leaves","flower","photosynthesis","Stem");
 
-
+-- ======================== Hard ========================
 insert into General_Knowledge values("51","H","What is the botanical name of Tomato?","Solanum Lycopersicum","Solanum Lycopersicuum","Solanium Lycopersicum","Solanam Lycopersiicum","Solanum Lycopersicum");
 insert into General_Knowledge values("52","H","What category of animal is an octopus?","Cephalopod","Sephalopod","Cefalopod","Solanam Lycopersiicum","Cephalopod");
 insert into General_Knowledge values("53","H","Your blood type is determined by the genes you inherit from your parents?","True","False","Dont know","Maybe","True");
 insert into General_Knowledge values("54","H","Common flowering plant studied to understand plant organization is?","Amaranthus","False","Dont know","Cephalopod","Amaranthus");
 insert into General_Knowledge values("55","H","The part of the stem where the `leaf is attached?","Node","mud","Dont know","stem","Node");
 insert into General_Knowledge values("56","H","What sensory organ of the human body consists of a visible part called the crown and unseen portion called the root?","Tooth","gums","Dont know","chicks","Tooth");
-insert into General_Knowledge values("57","H","Which is the largest human cell??","Tooth","skin","Dont know","chicks","Skin");
+insert into General_Knowledge values("57","H","Which is the largest human cell??","Tooth","skin","Dont know","chicks","skin");
 insert into General_Knowledge values("58","H","Who is widely considered to be the “Father of Mathematics?","Archimedes","sahil","vaibhav","Christopher Henry","Archimedes");
 insert into General_Knowledge values("59","H","Which number is considered as Ramanujan Number?","1729","0000","111","0318","1729");
 insert into General_Knowledge values("60","H","What is QAD?","Quod Erat Demonstrandum","Quartine at dadar","i am weak at maths","Dont know","Quartine at dadar");
@@ -113,7 +113,7 @@ insert into General_Knowledge values("89", "H","Saraswati Samman is given annual
 insert into General_Knowledge values("90", "H","Jesse Owens Global award is given in the field of","Literature","Journalism","Science","Sports","Sports");
 insert into General_Knowledge values("91", "H","Founder of Spring Framework?","CHarles Babage","Cristiano Messi","Sir Ravindra Jadeja","Sir Rod Johnson","Sir Rod Johnson");
 insert into General_Knowledge values("92", "H","Which of the following network protocols is described by GSM?","2G","3G","4G","5G","2G");
-insert into General_Knowledge values("93", "H"," Oncology is the study of?","Birds","Mammals","Cancer","Soil","Cancer");
+insert into General_Knowledge values("93", "H","Oncology is the study of?","Birds","Mammals","Cancer","Soil","Cancer");
 insert into General_Knowledge values("94", "H","The inventor of printing press was?","Marshall McLuhan","Ts' ai Lun","Johann Guttenberg","Frederic Barbier","Johann Guttenberg");
 insert into General_Knowledge values("95", "H","Who coined the term Biodiversity?","B. P. Singh","Karl Mobius","Walter G. Rosen","Sir A. G. Tansley","Walter G. Rosen");
 insert into General_Knowledge values("96", "H","First World climate conference was organised in?","Carbon-di-oxide","Chlorofluorocarbon","Nitro us oxide","Methane","Carbon-di-oxide");
@@ -121,5 +121,3 @@ insert into General_Knowledge values("97", "H","Who was the father of Indian Spa
 insert into General_Knowledge values("98", "H","Which of the following is Academic Institute Satellite?","CARTOSAT-2B","KALPANA-1"," INSAT-2E","SATYABAMASAT","SATYABAMASAT");
 insert into General_Knowledge values("99", "H","Which Geosynchronous Satellite Launch Vehicle was used by ISRO to launch Chandrayaan-2 space craft?","GSLV - MK III - M1","GSLV - MK II - M2","GSLV - MK IV - M8","GSLV - MKV - M4","GSLV - MK III - M1");
 insert into General_Knowledge values("100", "H","SRSS-1 satellite was launched by which country?","India","Bhutan","Nepal","Bangladesh","Bhutan");
-
-
